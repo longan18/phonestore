71d7a0adec5e8abd6cb1a7383dba666f69d4eead
18Dev <longan18.dev@gmail.com>
2024-05-12 21:11:44 +0700

    template admin

--- routes/admin.php
+++ routes/admin.php
@@ -7,3 +7,5 @@ use Illuminate\Support\Facades\Route;
 | Admin Routes
 |--------------------------------------------------------------------------
 */
+
+Route::view('/dashboard', 'admin.dashboard.index');
