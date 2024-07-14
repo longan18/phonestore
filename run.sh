5842ded5eb9abcdf5f1882165789d03627f9d98b
LongAn <longan18.dev@gmail.com>
2024-07-15 01:26:20 +0700

    docker

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# Chạy docker-compose up ở chế độ detached
+docker-compose up -d
+
+
+# Lấy tên container PHP (thay 'your-php-service' bằng tên service trong docker-compose.yml)
+CONTAINER_NAME=$(docker-compose ps -q app)
+
+
+# Chạy lệnh queue:work ẩn với nohup
+nohup docker exec -it $CONTAINER_NAME php artisan queue:work > /dev/null 2>&1 &
+nohup npm run dev > /dev/null 2>&1 &
